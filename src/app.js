8c89d65d3f07f885c899c0c9a93e6aeee0e7fde9
Shubham Ghadge <shubhamghadge784@gmail.com>
2024-06-12 15:29:29 +0530

    utils setup response error  asynchHandler

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import cookieParser from "cookie-parser";
+import express, { urlencoded } from "express";
+
+const app =  express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credential: true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export default app
+
+
+//use are used to set middlerware
